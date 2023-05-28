67cffa874a01082d8c470c31cdebcb9e7dcc9e86
Hemant Bansal <bhemant400@gmail.com>
2023-05-29 00:19:19 +0530

    All the files required

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter } from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import { AppContext } from "./context/contextApi";
+import Header from "./components/Header";
+import Feed from "./components/Feed";
+
+
+const App = () => {
+  return (    
+      <div className="flex flex-col h-full">
+      <BrowserRouter>
+      <Header />
+      </BrowserRouter>
+      <AppContext>
+
+      </AppContext>
+      
+      
+      </div>
+      
+    
+);
+};
 
 export default App;
